39e781369b504e22e3ff1065eba4525011a887ba
Radha Krishnan <35418017+RKrishnanTechie@users.noreply.github.com>
2020-12-13 15:53:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var textInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click",clickHandler);
+
+function clickHandler() {
+
+    console.log("clicked!");
+    console.log("input:",textInput.value );
+    console.log("HIII")
+};
+
+
